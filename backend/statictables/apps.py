2e1635fd95af452740e1f1a44cceaf5b372a8cde
velmuruganwebdev-crypto <veldv12@gmail.com>
2025-11-12 02:22:43 +0530

    Clean reset: only main apps + frontend

--- backend/statictables/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StatictablesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'statictables'
--- backend/statictables/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import StaticTable
-
-class StaticTableSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StaticTable
-        fields = '__all__'
